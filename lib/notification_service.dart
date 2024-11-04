9e1a752b5212545a557ad9d576faa79f3b77ec41
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2024-11-04 14:54:16 +0500

    feat: init notification_service

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'firebase_options.dart';
 
@@ -8,7 +11,33 @@ abstract class NotificationService {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+    try {
+      final fcm = await FirebaseMessaging.instance.getToken();
+      debugPrint('FCM: $fcm');
+      await Clipboard.setData(ClipboardData(text: fcm.toString()));
+    } on Exception {
+      debugPrint('FAILED TO GET FCM');
+    }
+    handleNotifications();
   }
 
   ///
+  static Future<void> handleNotifications() async {
+    /// foreground
+    FirebaseMessaging.onMessage.listen(
+      (message) {
+        debugPrint('Notification: foreground received $message');
+      },
+    );
+
+    /// background/terminated
+    FirebaseMessaging.onBackgroundMessage(_handleBackgroundNotification);
+  }
+}
+
+@pragma('vm:entry-point')
+Future<void> _handleBackgroundNotification(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
